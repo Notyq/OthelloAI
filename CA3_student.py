dd7e824607211442616c4b94b65411aff52b3d6f
Notyq <s10221789@connect.np.edu.sg>
2023-02-06 18:52:34 +0800

    time block
    time runs now

--- CA3_student.py
+++ CA3_student.py
@@ -10,6 +10,7 @@ from Globals import *
 import copy
 import threading
 
+
 class Othello(object):
 
     def __init__(self, world):
@@ -24,7 +25,8 @@ class Othello(object):
         self.started = False
 
         # generate the board
-        self.square_image = pygame.image.load("othello_square.png").convert_alpha()        
+        self.square_image = pygame.image.load(
+            "othello_square.png").convert_alpha()
 
         for i in range(1, 9):
             for j in range(1, 9):
@@ -41,15 +43,17 @@ class Othello(object):
         self.labelfont = pygame.font.SysFont("arial", 24, True)
         self.playerfont = pygame.font.SysFont("arial", 40, True)
 
-        self.black_disk_image = pygame.image.load("black_disk.png").convert_alpha()
+        self.black_disk_image = pygame.image.load(
+            "black_disk.png").convert_alpha()
         self.black_w, self.black_h = self.black_disk_image.get_size()
-        self.white_disk_image = pygame.image.load("white_disk.png").convert_alpha()
+        self.white_disk_image = pygame.image.load(
+            "white_disk.png").convert_alpha()
         self.white_w, self.white_h = self.white_disk_image.get_size()
-        self.cursor_image = pygame.image.load("cursor_disk.png").convert_alpha()
+        self.cursor_image = pygame.image.load(
+            "cursor_disk.png").convert_alpha()
 
         self.legal_moves = self.getMoves()
 
-     
     def getCopy(self):
         newcopy = Othello(self.world)
         for disk in self.disks.items():
@@ -61,7 +65,6 @@ class Othello(object):
         newcopy.legal_moves = copy.copy(self.legal_moves)
 
         return newcopy
-    
 
     def render(self, surface):
 
@@ -70,33 +73,38 @@ class Othello(object):
 
         for disk in list(self.disks.items()):
             if disk[1] == BLACK:
-                surface.blit(self.black_disk_image, Vector2(getPosition(disk[0]) - Vector2(self.black_w/2, self.black_h/2)))
+                surface.blit(self.black_disk_image, Vector2(getPosition(
+                    disk[0]) - Vector2(self.black_w/2, self.black_h/2)))
             elif disk[1] == WHITE:
-                surface.blit(self.white_disk_image, Vector2(getPosition(disk[0]) - Vector2(self.white_w/2, self.white_h/2)))
+                surface.blit(self.white_disk_image, Vector2(getPosition(
+                    disk[0]) - Vector2(self.white_w/2, self.white_h/2)))
 
         # Render the labels
         side_margin = LEFT_BORDER + TILE_SIZE/2
         column_label_y = TOP_BORDER + TILE_SIZE * 8 + 8
         for i in range(0, 8):
-            label = self.labelfont.render(str(chr(ord('a') + i)).replace("'", ""), True, (255, 255, 255))
+            label = self.labelfont.render(
+                str(chr(ord('a') + i)).replace("'", ""), True, (255, 255, 255))
             w, h = label.get_size()
-            surface.blit(label, (side_margin + TILE_SIZE * i - w/2, column_label_y))
+            surface.blit(
+                label, (side_margin + TILE_SIZE * i - w/2, column_label_y))
 
         row_label_x = LEFT_BORDER + TILE_SIZE * 8 + 12
         for i in range(0, 8):
             label = self.labelfont.render(str(8 - i), True, (255, 255, 255))
             w, h = label.get_size()
-            surface.blit(label, (row_label_x, TOP_BORDER + TILE_SIZE/2 + TILE_SIZE * i - h/2))
+            surface.blit(label, (row_label_x, TOP_BORDER +
+                         TILE_SIZE/2 + TILE_SIZE * i - h/2))
 
         # Show the player to move
         if not self.gameOver():
             if self.currentPlayer == BLACK:
                 label = self.playerfont.render("BLACK's turn", True, (0, 0, 0))
             else:
-                label = self.playerfont.render("WHITE's turn", True, (255, 255, 255))
+                label = self.playerfont.render(
+                    "WHITE's turn", True, (255, 255, 255))
 
             surface.blit(label, (410, 50))
-
 
     def makeMove(self, move):
 
@@ -110,11 +118,11 @@ class Othello(object):
             for y_offset in range(-1, 2):
                 if x_offset == 0 and y_offset == 0:
                     continue
-                
+
                 current_x = move[0]
                 current_y = move[1]
                 del opposing_disks[:]
-                
+
                 while True:
                     current_x += x_offset
                     current_y += y_offset
@@ -129,11 +137,10 @@ class Othello(object):
                     # follow line until own disk found
                     else:
                         if (current_x, current_y) in self.disks.keys():
-                            
+
                             # more opposing disks
                             if self.disks[(current_x, current_y)] != self.currentPlayer:
                                 opposing_disks.append((current_x, current_y))
-
 
                             # own disk found - add to disks_to_flip
                             else:
@@ -168,7 +175,6 @@ class Othello(object):
             self.legal_moves = self.getMoves()
 
             return self.getCopy()
-            
 
     def getMoves(self):
 
@@ -182,17 +188,17 @@ class Othello(object):
                 if (x_coord, y_coord) in self.disks.keys():
                     continue
 
-                for x_offset in range (-1, 2):
-                    for y_offset in range (-1, 2):
-                
+                for x_offset in range(-1, 2):
+                    for y_offset in range(-1, 2):
+
                         if x_offset == 0 and y_offset == 0:
                             continue
-                
+
                         current_x = x_coord
                         current_y = y_coord
                         first_disk_found = False        # Is the adjacent disk the opponent's?
                         legal_move_found = False
-                
+
                         while True:
                             current_x += x_offset
                             current_y += y_offset
@@ -207,11 +213,10 @@ class Othello(object):
                             # follow line until own disk found
                             else:
                                 if (current_x, current_y) in self.disks.keys():
-                            
+
                                     # more opposing disks
                                     if self.disks[(current_x, current_y)] != self.currentPlayer:
                                         continue
-
 
                                     # own disk found - add legal move to move_list
                                     else:
@@ -229,22 +234,24 @@ class Othello(object):
                         break
 
         return move_list
- 
 
     def gameOver(self):
         return False
 
-        
+
 class World(object):
 
     def __init__(self):
 
         self.entities = {}
         self.entity_id = 0
-        self.background = pygame.image.load("grass_bkgrd_1024_768.png").convert_alpha()
+        self.background = pygame.image.load(
+            "grass_bkgrd_1024_768.png").convert_alpha()
 
-        self.black_disk_image = pygame.image.load("black_disk.png").convert_alpha()
-        self.white_disk_image = pygame.image.load("white_disk.png").convert_alpha()
+        self.black_disk_image = pygame.image.load(
+            "black_disk.png").convert_alpha()
+        self.white_disk_image = pygame.image.load(
+            "white_disk.png").convert_alpha()
 
         self.othello = Othello(self)
 
@@ -285,7 +292,6 @@ class World(object):
             entity.render(surface)
 
 
-
 class GameEntity(object):
 
     def __init__(self, world, name, image):
@@ -299,7 +305,6 @@ class GameEntity(object):
 
         self.id = 0
 
-
     def render(self, surface):
 
         x, y = self.position
@@ -307,7 +312,6 @@ class GameEntity(object):
         draw_pos = Vector2(self.position.x - w/2, self.position.y - h/2)
 
         surface.blit(self.image, draw_pos)
-        
 
 
 class Square(GameEntity):
@@ -323,10 +327,12 @@ class Square(GameEntity):
 # Given coordinates, get the position of the centre of the tile corresponding to the coordinates
 def getPosition(coordinates):
 
-    return (LEFT_BORDER + (coordinates[0] - 1) * TILE_SIZE + TILE_SIZE / 2, \
+    return (LEFT_BORDER + (coordinates[0] - 1) * TILE_SIZE + TILE_SIZE / 2,
             (TOP_BORDER + TILE_SIZE * 8) - (coordinates[1] - 1) * TILE_SIZE - TILE_SIZE / 2)
 
 # Given the position, get the coordinates of the tile at that positiom
+
+
 def getCoordinates(position):
     return ((position[0] - LEFT_BORDER) // TILE_SIZE + 1, 8 - (position[1] - TOP_BORDER) // TILE_SIZE)
 
@@ -352,7 +358,8 @@ def alphabeta(board, player, maxDepth, currentDepth, alpha, beta):
         newBoard.makeMove(move)
 
         # Recurse
-        currentScore, currentMove = alphabeta(newBoard, 1 - player, maxDepth, currentDepth + 1, -beta, -alpha)
+        currentScore, currentMove = alphabeta(
+            newBoard, 1 - player, maxDepth, currentDepth + 1, -beta, -alpha)
 
         # update alpha
         if -currentScore > alpha:
@@ -366,7 +373,6 @@ def alphabeta(board, player, maxDepth, currentDepth, alpha, beta):
     return alpha, bestMove
 
 
-                    
 class AIThread (threading.Thread):
     def __init__(self, world, board):
         threading.Thread.__init__(self)
@@ -374,19 +380,19 @@ class AIThread (threading.Thread):
         self.board = board
 
     def run(self):
-        print ("Starting thread")
-        
+        print("Starting thread")
+
         search_depth = 4
         ai_score = -inf
         ai_move = None
 
-        ai_score, ai_move = alphabeta(self.board, self.world.othello.currentPlayer, search_depth, 0, -inf, inf)
+        ai_score, ai_move = alphabeta(
+            self.board, self.world.othello.currentPlayer, search_depth, 0, -inf, inf)
         print("Score is ", ai_score)
-                    
+
         self.world.othello.makeMove(ai_move)
 
         print("Exiting thread")
-
 
 
 def run():
@@ -401,9 +407,8 @@ def run():
     clock = pygame.time.Clock()
 
     game_started = False
-              
-    font = pygame.font.SysFont("arial", 24, True)
 
+    font = pygame.font.SysFont("arial", 24, True)
 
     while True:
 
@@ -421,7 +426,7 @@ def run():
                     ai_thread.start()
 
         if not world.othello.gameOver():
-            
+
             mouse_coord = getCoordinates(pygame.mouse.get_pos())
 
             if pygame.mouse.get_pressed()[0] == True and world.othello.currentPlayer == HUMAN and mouse_coord in world.othello.legal_moves:
@@ -435,59 +440,55 @@ def run():
 
             if world.othello.started and not world.othello.gameOver():
                 time_passed_seconds = time_passed / 1000
+                world.othello.timers[world.othello.currentPlayer] -= time_passed_seconds
 
             world.render(screen)
-        
-        # Show scores        
-        black_score = font.render("Black score: " + str(world.othello.scores[BLACK]), True, (0, 0, 0))
+
+        # Show scores
+        black_score = font.render(
+            "Black score: " + str(world.othello.scores[BLACK]), True, (0, 0, 0))
         screen.blit(black_score, (50, 50))
-        white_score = font.render("White score: " + str(world.othello.scores[WHITE]), True, (255, 255, 255))
+        white_score = font.render(
+            "White score: " + str(world.othello.scores[WHITE]), True, (255, 255, 255))
         screen.blit(white_score, (840, 50))
 
         # Show timers
-        black_timer = font.render("Time Left: " + "{:.1f}".format(world.othello.timers[BLACK]), True, (0, 0, 0))
+        black_timer = font.render(
+            "Time Left: " + "{:.1f}".format(world.othello.timers[BLACK]), True, (0, 0, 0))
         screen.blit(black_timer, (50, 100))
-        white_timer = font.render("Time Left: " + "{:.1f}".format(world.othello.timers[WHITE]), True, (255, 255, 255))
+        white_timer = font.render(
+            "Time Left: " + "{:.1f}".format(world.othello.timers[WHITE]), True, (255, 255, 255))
         screen.blit(white_timer, (840, 100))
 
         if not world.othello.started:
-            msg = world.othello.playerfont.render("Hit <SPACE BAR> to play White", True, (255, 255, 0))
+            msg = world.othello.playerfont.render(
+                "Hit <SPACE BAR> to play White", True, (255, 255, 0))
             w, h = msg.get_size()
             screen.blit(msg, (SCREEN_WIDTH/2 - w/2, SCREEN_HEIGHT - 80))
 
-
         if world.othello.gameOver():
             if world.othello.timers[WHITE] <= 0.:
-                win_msg = world.othello.playerfont.render("BLACK WINS ON TIME! ", True, (0, 0, 0))
+                win_msg = world.othello.playerfont.render(
+                    "BLACK WINS ON TIME! ", True, (0, 0, 0))
             elif world.othello.timers[BLACK] <= 0.:
-                win_msg = world.othello.playerfont.render("WHITE WINS ON TIME! ", True, (255, 255, 255))
+                win_msg = world.othello.playerfont.render(
+                    "WHITE WINS ON TIME! ", True, (255, 255, 255))
             elif world.othello.scores[BLACK] > world.othello.scores[WHITE]:
-                win_msg = world.othello.playerfont.render("BLACK WINS! ", True, (0, 0, 0))
+                win_msg = world.othello.playerfont.render(
+                    "BLACK WINS! ", True, (0, 0, 0))
             elif world.othello.scores[BLACK] < world.othello.scores[WHITE]:
-                win_msg = world.othello.playerfont.render("WHITE WINS!", True, (255, 255, 255))
+                win_msg = world.othello.playerfont.render(
+                    "WHITE WINS!", True, (255, 255, 255))
             else:
-                win_msg = world.othello.playerfont.render("DRAW", True, (127, 127, 127))
+                win_msg = world.othello.playerfont.render(
+                    "DRAW", True, (127, 127, 127))
 
             w, h = win_msg.get_size()
 
             screen.blit(win_msg, (SCREEN_WIDTH/2 - w/2, 50))
 
-        
         pygame.display.update()
 
-        
-        
 
 if __name__ == "__main__":
     run()
-
-
-
-
-        
-        
-
-
-
-
-        
